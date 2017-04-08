bdcc24d365460fb0a0e3d864dcb66eb8010721b0
creamidea <creamidea@gmail.com>
2017-04-09 02:58:49 +0800

    feat: add function to set python executable (but not use)

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -3,6 +3,13 @@
 		("SConscript\\'" . python-mode))
               auto-mode-alist))
 
+;; https://github.com/gregsexton/ob-ipython/issues/28#issuecomment-182346001
+;; (setq python-shell-completion-native-enable nil)
+;; using python3: http://stackoverflow.com/a/17139416/1925954
+;; (setq python-version-checked t)
+;; (setq python-shell-interpreter "python3")
+;; or you can set by customize (M-x customize)
+
 (require-package 'pip-requirements)
 
 (when (maybe-require-package 'anaconda-mode)
