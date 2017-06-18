620a576e30673afa5a3d96b079c24e3e7b9ef448
creamidea <creamidea@gmail.com>
2017-06-18 11:03:50 +0800

    feat: replace the offical source with tuna
    + melpa
    + gnu
    + org

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -23,24 +23,23 @@
 
 
 ;;; Standard package repositories
-
-;; We include the org repository for completeness, but don't normally
-;; use it.
-;; http://orgmode.org/elpa/
-(add-to-list 'package-archives '("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/"))
-
-(when (< emacs-major-version 24)
-  ;; http://elpa.gnu.org/packages/
-  (add-to-list 'package-archives '("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
+;; 全部使用 tuna 镜像代理
 
 (defconst sanityinc/no-ssl (and (memq system-type '(windows-nt ms-dos))
                                 (not (gnutls-available-p))))
 
-;;; Also use Melpa for most packages
 (add-to-list 'package-archives
              `("melpa" . ,(if sanityinc/no-ssl
                               "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/"
                             "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(add-to-list 'package-archives
+             `("gnu" . ,(if sanityinc/no-ssl
+                            "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/"
+                          "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
+(add-to-list 'package-archives
+             `("org" . ,(if sanityinc/no-ssl
+                            "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/"
+                          "https://mirrors.tuna.tsinghua.edu.cn/elpa/org/")))
 
 (unless sanityinc/no-ssl
   ;; Force SSL for GNU ELPA
@@ -48,7 +47,6 @@
 
 ;; NOTE: In case of MELPA problems, the official mirror URL is
 ;; https://www.mirrorservice.org/sites/stable.melpa.org/packages/
-
 
 
 
